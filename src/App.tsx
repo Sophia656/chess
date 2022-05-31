12d322d8e12c4e9134b281fc48045b25fba63a99
sophia656 <antropovasophia@gmail.com>
2022-05-31 13:09:12 +0300

    figures

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import './App.css'
+import BoardComponent from './components/BoardComponent';
+import { Board } from './models/Board';
+
+const App = () => {
+  const [board, setBoard] = useState(new Board())
+
+  useEffect(() => {
+    restart()
+  }, [])
+
+  // начать новую партию
+  function restart() {
+    const newBoard = new Board();
+    newBoard.initCells()
+    newBoard.addFigures()
+    setBoard(newBoard)
+  }
+  return (
+    <div className='app'>
+      <BoardComponent board={board} setBoard={setBoard} />
+    </div>
+  );
+};
+
+export default App;
